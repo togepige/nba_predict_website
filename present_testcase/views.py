0950cd3821609d265d857d3c45d4fb3f911333b9
Junkai Yang <cyfloel0516@hotmail.com>
2016-12-10 20:15:07 -0500

    add base django app

---
+++ present_testcase/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template('present_testcase/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
